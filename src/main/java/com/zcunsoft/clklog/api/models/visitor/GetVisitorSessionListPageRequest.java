793f426d31ac1a2cdf1b1a1e2ca9dd5be79c803e
Max Cheng <junchengchn@gmail.com>
2024-05-23 11:32:44 +0800

    <bug>修复visitor session跨项目统计的bug

--- src/main/java/com/zcunsoft/clklog/api/models/visitor/GetVisitorSessionListPageRequest.java
+++ src/main/java/com/zcunsoft/clklog/api/models/visitor/GetVisitorSessionListPageRequest.java
@@ -7,12 +7,11 @@ import lombok.Data;
 @Data
 public class GetVisitorSessionListPageRequest {
 
-    @Schema(description = "页码", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
-    private int pageNum;
+	@Schema(description = "页码", requiredMode = Schema.RequiredMode.REQUIRED, example = "1")
+	private int pageNum;
 
-    @Schema(description = "页长", requiredMode = Schema.RequiredMode.REQUIRED, example = "50")
-    private int pageSize;
-
+	@Schema(description = "页长", requiredMode = Schema.RequiredMode.REQUIRED, example = "50")
+	private int pageSize;
 
 //    @Schema(description = "渠道", requiredMode = Schema.RequiredMode.REQUIRED, example = "[\"安卓\",\"苹果\",\"网站\",\"微信小程序\"]")
 //    private List<String> channel = new ArrayList<>();
@@ -23,17 +22,17 @@ public class GetVisitorSessionListPageRequest {
 //    @Schema(description = "地域", requiredMode = Schema.RequiredMode.REQUIRED, example = "[\"上海\",\"北京\",\"其他\"]")
 //    private List<String> province;
 //
-//    @Schema(description = "访客类型", requiredMode = Schema.RequiredMode.REQUIRED, example = "新访客")
+//    @Schema(description = "访客类型", requiredMode = Schema.RequiredMode.REQUIRED, example = "新访客/老访客")
 //    private String visitorType;
 //
 //    @Schema(description = "开始时间", requiredMode = Schema.RequiredMode.REQUIRED, example = "2023-06-08")
 //    private String startTime;
 //    @Schema(description = "结束时间", requiredMode = Schema.RequiredMode.REQUIRED, example = "2023-06-10")
 //    private String endTime;
-//
-//    @Schema(description = "应用名", requiredMode = Schema.RequiredMode.REQUIRED, example = "")
-//    private String projectName;
-    
-    @Schema(description = "用户ID", requiredMode = Schema.RequiredMode.REQUIRED, example = "8609475f862bd2cc")
-    private String distinctId;
+
+	@Schema(description = "应用名", requiredMode = Schema.RequiredMode.REQUIRED, example = "clklog")
+	private String projectName;
+
+	@Schema(description = "用户ID", requiredMode = Schema.RequiredMode.REQUIRED, example = "8609475f862bd2cc")
+	private String distinctId;
 }
